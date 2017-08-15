886729570932760248866087b1e237ab7222d8ea
Natnael getachew <natwinet@gmail.com>
2017-08-15 16:53:39 -0500

    updated js feature with module pattern

--- build/web/asset/dict.js
+++ build/web/asset/dict.js
@@ -1,58 +1,64 @@
 "use strict";
-(function(){
+
 $(document).ready(function(){
-  
+    
+    var Module = (function(){
+        
+        var ajaxFailure = function(){
+            $('#errors').html('<p class="bg-danger">An ajax error occurred.</p>');
+        };
+    
+        var ajaxSuccess = function(data) {
+            //console.log(data);
+            $("#result-wrap").empty();
+            if(data.results[0]  !== undefined ){
+                for(var i=0; i<data.results.length; i++){
+                    var wordType = (data.results[i].wordtype != "") ? " ("+ data.results[i].wordtype+ ")  " : " ";
+                    $("#result-wrap").append("<div class='result'>" + i + wordType +":: "+data.results[i].definition + "</div>");
+                }
+            }
+            else{
+                $("#result-wrap").html('<p class="bg-danger">No result found</p>'); 
+            }
+        };
+        
+        var searchTerm = function(){
+            if($("#term").val() == ""){
+                $("#result-wrap").html('<p class="bg-danger">You need to enter Term!</p>'); 
+                return;
+            }
+            
+            $.get("http://localhost:8080/Online-Dictionary-Project/dictServlet?inpTerm=" + $("#term").val(), 
+            function(data, status){
+                if(status == "success"){
+                    ajaxSuccess(data);
+                }
+                if(status == "error"){
+                    ajaxFailure();
+                }
+            
+            });
+        };
+        
+        return {
+            ajaxFailure : ajaxFailure,
+            ajaxSuccess : ajaxSuccess,
+            searchTerm : searchTerm
+        };
+    
+    })();
+    
     $(document).ajaxStart(function () {
         $("#result-wrap").text("Loading..."); 
-    }).ajaxStop(function () {
-       
-    });
+    }).ajaxStop(function () { });
    
-    function ajaxSuccess(data) {
-        //console.log(data);
-        $("#result-wrap").empty();
-        if(data.results[0]  !== undefined ){
-            for(var i=0; i<data.results.length; i++){
-                var wordType = (data.results[i].wordtype != "") ? " ("+ data.results[i].wordtype+ ")  " : " ";
-                $("#result-wrap").append("<div class='result'>" + i + wordType +":: "+data.results[i].definition + "</div>");
-            }
-        }
-        else{
-            $("#result-wrap").html('<p class="bg-danger">No result found</p>'); 
-        }
-    }
-    
-    function ajaxFailure(){
-        $('#errors').html('<p class="bg-danger">An ajax error occurred.</p>');
-    }
-    
-    function searchTerm(){
-        if($("#term").val() == ""){
-            $("#result-wrap").html('<p class="bg-danger">You need to enter Term!</p>'); 
-            return;
-        }
-            
-        $.get("http://localhost:8080/Online-Dictionary-Project/dictServlet?inpTerm=" + $("#term").val(), 
-        function(data, status){
-            if(status == "success"){
-                ajaxSuccess(data);
-            }
-            if(status == "error"){
-                ajaxFailure();
-            }
-            
-        });
-    }
-    
-    $("#lookup-btn").click(searchTerm);
+    $("#lookup-btn").click(Module.searchTerm);
 
     $( "#term" ).keypress(function( event ) {
         if ( event.which == 13 ) { // when enter is pressed
-            searchTerm();
+            Module.searchTerm();
             //event.preventDefault();
         }
     });
 
 });
-})();
-
