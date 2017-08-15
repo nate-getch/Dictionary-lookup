ba15e5bb3ffb475fcc78493b3e5afddb70c02262
Natnael <natwinet@gmail.com>
2017-08-15 02:02:58 -0500

    updated the dictionary project with proper implementation

--- src/java/DictServlet.java
+++ src/java/DictServlet.java
@@ -1,8 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -10,13 +10,16 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+import java.util.ArrayList;
+import org.json.simple.JSONObject;
 import connection.DbConnection;
 /**
  *
  * @author Natnael Getachew
  */
 public class DictServlet extends HttpServlet {
-    private DbConnection dbConn = new DbConnection();
+    private final DbConnection con = new DbConnection();
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -28,27 +31,54 @@ public class DictServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-      
-        response.setContentType("text/html;charset=UTF-8");      
+        String inpTerm = request.getParameter("inpTerm").toString().trim();
+        //System.out.println(inpTerm);
+        
+        if(inpTerm == ""){
+            return;
+        }
+        response.setContentType("application/json;charset=UTF-8");
+        Statement stmt;
+        Connection c=con.openConnection();
+        //if(con.openConnection() != null)
+        //out.println("yesss Connected");
+        JSONObject jsonObj = new JSONObject();
         
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet DictServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
+
+            try{
+                // Execute query
+                stmt = c.createStatement();
+                String sql;
+                sql = "SELECT * FROM entries WHERE word = '"+inpTerm+"'";
+                ResultSet rs = stmt.executeQuery(sql);
+                ArrayList list = new ArrayList();
+                
+                //Extract data from result set
+                while(rs.next()){
+                    //Retrieve by column name
+                    JSONObject tempObj = new JSONObject();
+                    tempObj.put("word", rs.getString("word"));
+                    tempObj.put("wordtype", rs.getString("wordtype"));
+                    tempObj.put("definition", rs.getString("definition"));
+                    list.add(tempObj);                    
+                }
+
+                jsonObj.put("results", list);
+                // Clean up environment
+                rs.close();
+                stmt.close();
+                con.closeConnection(c);
+            }catch(SQLException se){
+                //Handle errors for JDBC
+                se.printStackTrace();
+            }
             
-            if(dbConn.openConnection() != null)
-                out.println("yesss Connected");
-                    
-            out.println("<h1>Servlet DictServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            out.println(jsonObj);
+            
         }
     }
-
+    
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -63,7 +93,7 @@ public class DictServlet extends HttpServlet {
             throws ServletException, IOException {
         processRequest(request, response);
     }
-
+    
     /**
      * Handles the HTTP <code>POST</code> method.
      *
@@ -77,7 +107,7 @@ public class DictServlet extends HttpServlet {
             throws ServletException, IOException {
         processRequest(request, response);
     }
-
+    
     /**
      * Returns a short description of the servlet.
      *
@@ -87,5 +117,5 @@ public class DictServlet extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-
+    
 }
--- web/asset/dict.js
+++ web/asset/dict.js
@@ -1,2 +1,58 @@
+"use strict";
+(function(){
+$(document).ready(function(){
+  
+    $(document).ajaxStart(function () {
+        $("#result-wrap").text("Loading..."); 
+    }).ajaxStop(function () {
+       
+    });
+   
+    function ajaxSuccess(data) {
+        //console.log(data);
+        $("#result-wrap").empty();
+        if(data.results[0]  !== undefined ){
+            for(var i=0; i<data.results.length; i++){
+                var wordType = (data.results[i].wordtype != "") ? " ("+ data.results[i].wordtype+ ")  " : " ";
+                $("#result-wrap").append("<div class='result'>" + i + wordType +":: "+data.results[i].definition + "</div>");
+            }
+        }
+        else{
+            $("#result-wrap").html('<p class="bg-danger">No result found</p>'); 
+        }
+    }
+    
+    function ajaxFailure(){
+        $('#errors').html('<p class="bg-danger">An ajax error occurred.</p>');
+    }
+    
+    function searchTerm(){
+        if($("#term").val() == ""){
+            $("#result-wrap").html('<p class="bg-danger">You need to enter Term!</p>'); 
+            return;
+        }
+            
+        $.get("http://localhost:8080/Online-Dictionary-Project/dictServlet?inpTerm=" + $("#term").val(), 
+        function(data, status){
+            if(status == "success"){
+                ajaxSuccess(data);
+            }
+            if(status == "error"){
+                ajaxFailure();
+            }
+            
+        });
+    }
+    
+    $("#lookup-btn").click(searchTerm);
 
+    $( "#term" ).keypress(function( event ) {
+        if ( event.which == 13 ) { // when enter is pressed
+            searchTerm();
+            //event.preventDefault();
+        }
+    });
+
+});
+})();
 
